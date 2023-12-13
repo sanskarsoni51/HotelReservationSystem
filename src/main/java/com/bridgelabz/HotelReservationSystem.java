72b4624b6d475e8c1767292fe17d5a94f85033aa
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-12-13 17:12:23 +0530

    Added: Functionality to add hotels with name and rates

--- src/main/java/com/bridgelabz/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -1,16 +1,22 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+
 public class HotelReservationSystem {
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System");
+
+        ArrayList<Hotels> hotelList = new ArrayList<>();
 
         Hotels Lakewood = new Hotels("Lakewood",110);
         Hotels Bridgewood = new Hotels("Bridgewood",160);
         Hotels Ridgewood = new Hotels("Ridgewood",220);
 
-        System.out.println(Lakewood.toString());
-        System.out.println(Bridgewood.toString());
-        System.out.println(Ridgewood.toString());
+        hotelList.add(Lakewood);
+        hotelList.add(Bridgewood);
+        hotelList.add(Ridgewood);
+
+        System.out.println(hotelList);
 
     }
 }
