caee86fe646c9a4299cd8b565f9d62d7e15f4111
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-12-14 11:38:22 +0530

    Added: Functionality to take range of dates and provide them with the cheapest hotel based on it is a weekend or weekday

--- src/main/java/com/bridgelabz/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -20,23 +20,40 @@ public class HotelReservationSystem {
 
         System.out.println(hotelList);
         Scanner sc = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.println("Enter number of days: ");
         int noOfDays = sc.nextInt();
         Set<String> Stay = new HashSet<String>();
 //        List<String> Stay = new Vector<String>();
         int TotalRate = 0;
+        System.out.println("Enter dates: ");
+        String dateInput;
         for (int i = 0; i <noOfDays; i++) {
-            String dateInput = sc.nextLine();
-            String h = hotelList.stream().min(Comparator.comparing(Hotels::getRateForRegular)).get().getName();
-            Stay.add(h);
-            int rate = hotelList.stream().min(Comparator.comparing(Hotels::getRateForRegular)).get().getRateForRegular();
-            TotalRate = TotalRate+rate;
-//            boolean weekEnd = DateInput.IsWeekend(DateInput.DateFormat(dateInput));
-//            System.out.println(weekEnd);
+            dateInput = s.nextLine();
+            // Parsing the user input date string and checking if it's a weekend
+            boolean weekEnd = DateInput.IsWeekend(DateInput.DateFormat(dateInput));
+
+            // Choosing a hotel based on whether it's a weekend or not
+            if(weekEnd){
+                // Choosing the hotel with the minimum rate for weekends
+                String h = hotelList.stream().min(Comparator.comparing(Hotels::getRateForWeekends)).get().getName();
+                Stay.add(h);
+                // Adding the rate of the selected hotel for weekends to the total rate
+                int rate = hotelList.stream().min(Comparator.comparing(Hotels::getRateForWeekends)).get().getRateForWeekends();
+                TotalRate = TotalRate+rate;
+            }else {
+                // Choosing the hotel with the minimum rate for regular days
+                String h = hotelList.stream().min(Comparator.comparing(Hotels::getRateForRegular)).get().getName();
+                Stay.add(h);
+                // Adding the rate of the selected hotel for regular days to the total rate
+                int rate = hotelList.stream().min(Comparator.comparing(Hotels::getRateForRegular)).get().getRateForRegular();
+                TotalRate = TotalRate + rate;
+            }
+
         }
         for (String i:Stay
              ) {
-            System.out.print(i + " ");
+            System.out.print(i + " and ");
         }
         System.out.print("With Total rate $"+TotalRate);
 
