cb51cecdc0cc186419da108b8838eacc1e678069
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-12-14 15:50:42 +0530

    Added:comments Added

--- src/main/java/com/bridgelabz/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -8,6 +8,7 @@ import static java.lang.Math.min;
 
 public class HotelReservationSystem {
     public static void main(String[] args) throws ParseException {
+        // Constants for different hotel selection options
         final int CheapestHotels = 1;
         final int CheapestBestRated = 2;
         final int BestRated = 3;
@@ -15,35 +16,46 @@ public class HotelReservationSystem {
 
         System.out.println("Welcome to Hotel Reservation System");
 
+        // Create a list to store hotel objects
         ArrayList<Hotels> hotelList = new ArrayList<>();
 
-        Hotels Lakewood = new Hotels("Lakewood", 110, 90, 3,80,80);
-        Hotels Bridgewood = new Hotels("Bridgewood", 150, 50, 4,50,110);
-        Hotels Ridgewood = new Hotels("Ridgewood", 220, 150, 5,40,100);
+        // Initialize hotel objects and add them to the list
+        Hotels Lakewood = new Hotels("Lakewood", 110, 90, 3, 80, 80);
+        Hotels Bridgewood = new Hotels("Bridgewood", 150, 50, 4, 50, 110);
+        Hotels Ridgewood = new Hotels("Ridgewood", 220, 150, 5, 40, 100);
 
         hotelList.add(Lakewood);
         hotelList.add(Bridgewood);
         hotelList.add(Ridgewood);
 
+        // Display the list of hotels
         System.out.println(hotelList);
+
+        // Set up scanners for user input
         Scanner sc = new Scanner(System.in);
         Scanner s = new Scanner(System.in);
 
+        // Set to store selected hotels, and variable to track total rate
         Set<String> Stay = new HashSet<>();
-//        List<String> Stay = new Vector<String>();
         int TotalRate = 0;
+
+        // Display menu options
         System.out.println("1)Cheapest Hotels \n 2)Cheapest Hotel with best Rating \n 3)Best Rated Hotel \n 4)Cheapest Hotel with best Rating for Reward customer");
         int option = sc.nextInt();
 
+        // Get the number of days for the stay
         System.out.println("Enter number of days: ");
         int noOfDays = sc.nextInt();
+
+        // Get dates for the stay
         System.out.println("Enter dates: ");
         String dateInput;
 
+        // Switch statement to handle different hotel selection options
         switch (option) {
 
             case CheapestHotels:
-
+                // Logic for selecting cheapest hotels based on weekdays or weekends
                 for (int i = 0; i < noOfDays; i++) {
                     dateInput = s.nextLine();
                     // Parsing the user input date string and checking if it's a weekend
@@ -66,35 +78,35 @@ public class HotelReservationSystem {
                         TotalRate = TotalRate + rate;
                     }
                 }
+                break;
 
             case CheapestBestRated:
+                // Logic for selecting cheapest hotel with the best rating
                 int week_end = 0;
-                int r;
                 for (int i = 0; i < noOfDays; i++) {
                     dateInput = s.nextLine();
                     // Parsing the user input date string and checking if it's a weekend
                     boolean weekEnd = DateInput.IsWeekend(DateInput.DateFormat(dateInput));
-                    if(weekEnd) week_end++;
+                    if (weekEnd) week_end++;
                 }
                 int min = 0;
                 String htel = "";
-                for (Hotels hotel:hotelList) {
-
-                    r = (hotel.getRateForRegular()*(noOfDays-week_end)) +(hotel.getRateForWeekends()*week_end);
-                    int ratingByRate = r/ hotel.getRatings();
-                    if(min<ratingByRate){
+                for (Hotels hotel : hotelList) {
+                    // Calculate the total rate based on the hotel's rates and ratings
+                    int r = (hotel.getRateForRegular() * (noOfDays - week_end)) + (hotel.getRateForWeekends() * week_end);
+                    int ratingByRate = r / hotel.getRatings();
+                    // Choose the hotel with the minimum rating-by-rate value
+                    if (min < ratingByRate) {
                         min = r;
                         htel = hotel.getName();
                     }
-
                 }
                 TotalRate = min;
                 Stay.add(htel);
                 break;
-//                code will be implemented
-
 
             case BestRated:
+                // Logic for selecting the best-rated hotel based on weekdays or weekends
                 for (int i = 0; i < noOfDays; i++) {
                     dateInput = s.nextLine();
                     // Parsing the user input date string and checking if it's a weekend
@@ -117,40 +129,40 @@ public class HotelReservationSystem {
                         TotalRate = TotalRate + rate;
                     }
                 }
+                break;
 
             case CheapestBeatRatedForRewardCustomer:
+                // Logic for selecting cheapest hotel with the best rating for reward customers
                 int WEnd = 0;
-                int price;
                 for (int i = 0; i < noOfDays; i++) {
                     dateInput = s.nextLine();
                     // Parsing the user input date string and checking if it's a weekend
                     boolean weekEnd = DateInput.IsWeekend(DateInput.DateFormat(dateInput));
-                    if(weekEnd) WEnd++;
+                    if (weekEnd) WEnd++;
                 }
-                int min1 =10000;
+                int min1 = 10000;
                 String htel1 = "";
-                for (Hotels hotel:hotelList) {
-
-                    price = (hotel.getRateForRewardCustomerRegular()*(noOfDays-WEnd)) +(hotel.getRateForRewardCustomerWeekends()*WEnd);
-                    int ratingByRate = price/ hotel.getRatings();
-                    if(min1>ratingByRate){
+                for (Hotels hotel : hotelList) {
+                    // Calculate the total rate based on the hotel's rates and ratings for reward customers
+                    int price = (hotel.getRateForRewardCustomerRegular() * (noOfDays - WEnd)) + (hotel.getRateForRewardCustomerWeekends() * WEnd);
+                    int ratingByRate = price / hotel.getRatings();
+                    // Choose the hotel with the minimum rating-by-rate value
+                    if (min1 > ratingByRate) {
                         min1 = price;
                         htel1 = hotel.getName();
                     }
-
                 }
                 TotalRate = min1;
                 Stay.add(htel1);
                 break;
-
-
         }
 
+        // Display selected hotels and total rate
         for (String i : Stay) {
             System.out.print(i + " and ");
         }
         System.out.print("With Total rate $" + TotalRate);
-
     }
 }
+
 
